b1020f816c5a42be0078a0c2b7c333504a5ed5a6
djq <441466583@qq.com>
2021-01-16 18:08:18 +0800

    搭建Demo 未完成

--- spring_security_demo/src/main/java/cn/itsource/spring_security_demo/config/SecurityConfig.java
+++ spring_security_demo/src/main/java/cn/itsource/spring_security_demo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package cn.itsource.spring_security_demo.config;
 
+import cn.itsource.spring_security_demo.service.MyUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * @desciption:用途
@@ -18,7 +23,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private UserDetailsService userDetailsService;
+    private MyUserDetailService userDetailsService;
+    //由于rememberMe功能需要连接数据库存储token信息，因此需要注入数据库链接信息
+    @Autowired
+    private DataSource dataSource;
+    //配置对象
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        //jdbcTokenRepository.setCreateTableOnStartup(true);//请求方法的时候创建persistent_logins表
+        return jdbcTokenRepository;
+    }
 
     @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -38,17 +54,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
       http.exceptionHandling().accessDeniedPage("/unAuth.html"); //设置无权限时用户访问的页面
-
+      http.logout().logoutUrl("/logout").logoutSuccessUrl("/test/hello").permitAll();//添加用户注销功能
       http.formLogin()  //自定义自己编写的登录页面
               .loginPage("/login.html")          //登录页面设置
               .loginProcessingUrl("/user/login")  //登录访问路径
-              .defaultSuccessUrl("/test/index").permitAll()//登陆成功之后，跳转路径
+              /*.defaultSuccessUrl("/test/index").permitAll()//登陆成功之后，跳转路径*/
+              .defaultSuccessUrl("/success.html").permitAll()
               .and().authorizeRequests().antMatchers("/","/test/hello","/user/login").permitAll()//设置哪些路径不需要认证
               //.anyRequest().authenticated()//所有的请求都已验证
               //.antMatchers("/test/index").hasAuthority("admins")//拦截/test/index请求验证该用户是否是admins权限
               //.antMatchers("/test/index").hasAnyAuthority("admins,manage")//拦截/test/index请求验证该用户是否含有admins或manage权限
               //.antMatchers("/test/index").hasRole("sale")//拦截/test/index请求验证该用户是否是sale角色
               .antMatchers("/test/index").hasAnyRole("sale,manager")//拦截/test/index请求验证该用户是否含有sale角色
+              .and().rememberMe().tokenRepository(persistentTokenRepository())//配置rememberMe，注入数据库链接信息
+              .tokenValiditySeconds(60)//配置用户登录有效时长，单位为秒
+              .userDetailsService(userDetailsService)//注入当前使用的userDetailsService对象,如果没有代码默认为Security自带的UserDetailsService
               .and().csrf().disable(); //关闭csrf防护
     }
 
--- spring_security_demo/src/main/java/cn/itsource/spring_security_demo/controller/HelloController.java
+++ spring_security_demo/src/main/java/cn/itsource/spring_security_demo/controller/HelloController.java
@@ -1,8 +1,12 @@
 package cn.itsource.spring_security_demo.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @desciption:用途
  * @author: 杜俊圻
@@ -12,11 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test")
 public class HelloController {
     @GetMapping(value = "/hello")
-    public String list(){
+    public String list() {
         return "hello";
     }
+
     @GetMapping(value = "/index")
-    public String index(){
+    public String index() {
+        return "hello index";
+    }
+
+    @GetMapping(value = "/update")
+    //@Secured({"ROLE_sale", "ROLE_manager"})//注解类型管理controller中的方法，存在该用户权限的用户才能访问
+    //@PreAuthorize(value = "hasAnyAuthority('sale')")//Spring Security的post前注释,在访问该方法访问前验证是否存在该权限
+    @PostAuthorize(value = "hasAnyAuthority('sale')")//Spring Security的post后注释,在访问该方法访问后验证是否存在该权限，不常用。应用场景在方法需要返回值的时候才应用
+    public String update() {
+        System.out.println("update......");
         return "hello index";
     }
 }
